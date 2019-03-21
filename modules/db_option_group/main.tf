ee1b24e2b0ec3ff618f9b65535d813a8c1a57ebf
kanewinter <mentatmind@gmail.com>
2019-03-20 18:11:00 -0600

    Update main.tf

--- modules/db_option_group/main.tf
+++ modules/db_option_group/main.tf
@@ -1,5 +1,5 @@
 resource "aws_db_option_group" "this" {
-  count = "${var.create ? 1 : 0}"
+  count = 0
 
   name_prefix              = "${var.name_prefix}"
   option_group_description = "${var.option_group_description == "" ? format("Option group for %s", var.identifier) : var.option_group_description}"
