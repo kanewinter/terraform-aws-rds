04695cb9b230f3a172444b73f77110c3dd5a6611
kanewinter <mentatmind@gmail.com>
2019-03-22 19:01:49 -0600

    Update main.tf

--- modules/db_subnet_group/main.tf
+++ modules/db_subnet_group/main.tf
@@ -1,5 +1,5 @@
 resource "aws_db_subnet_group" "this" {
-  count = 0
+  count = "${var.create ? 1 : 0}"
 
   name_prefix = "${var.name_prefix}"
   description = "Database subnet group for ${var.identifier}"
